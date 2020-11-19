9d9f258e3a2d7da7fa7b27b4d27703339610ffbf
Poplawski, Krzysztof <kpoplawski@rxsense.com>
2020-11-19 22:22:45 +0100

    view stubs

---
+++ src/views/List/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './List.css';
+
+const List = () => {
+  return (
+
+  )
+}
+
+export default List;
---
+++ src/views/Map/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Map.css';
+
+const Map = () => {
+  return (
+
+  )
+}
+
+export default Map;
